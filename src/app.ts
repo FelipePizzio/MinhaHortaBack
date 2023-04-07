9ce003ea178fc7864b27dd76d127a6cd27b08b86
Felipe Pizzio <felipetpizzio@gmail.com>
2023-04-07 20:39:08 -0300

    feat: configuração inicial da estrutura do projeto

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
+
+export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
+
+async function main() {
+  // Connect the client
+
+  await prisma.$connect()
+
+  // ... you will write your Prisma Client queries here
+
+  const allUsers = await prisma.user.findMany()
+
+  console.log(allUsers)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+
+  .catch(async (e) => {
+    console.error(e)
+
+    await prisma.$disconnect()
+
+    process.exit(1)
+  })
